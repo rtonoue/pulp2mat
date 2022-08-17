8fa84abdf30434615faa4eff993cec69e54e1d10
rtonoue <rtonoue625@gmail.com>
2022-08-17 13:58:50 +0900

    update: for pypi

--- pulp2mat/__init__.py
+++ pulp2mat/__init__.py
@@ -1,5 +1,14 @@
-import pulp as pl
-import numpy as np
+import os, glob
 
-# from scipy.optimize import milp, LinearConstraint, Bounds
+__copyright__ = "Copyright (C) 2018 Your Name"
+__version__ = "1.0.0"
+__license__ = "MIT"
+__author__ = "Ryota Tonoue"
+__author_email__ = "rtonoue625@gmail.com"
+__url__ = "https://github.com/rtonoue/pulp2mat"
+__all__ = [
+    os.path.split(os.path.splitext(file)[0])[1]
+    for file in glob.glob(os.path.join(os.path.dirname(__file__), "[a-zA-Z0-9]*.py"))
+]
+
 from pulp2mat.lp2mat import *
